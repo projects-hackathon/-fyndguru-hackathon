157ac6076513f390b8d5432372eb4970bdfc248b
Adarsha-H-S <adarshahs95@gmail.com>
2020-09-12 17:55:38 +0530

    Added handler for ping and health endpoints

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+        "log"
+        "context"
+
+        "github.com/aws/aws-lambda-go/events"
+        "github.com/aws/aws-lambda-go/lambda"
+        "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+        "github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+        // stdout and stderr are sent to AWS CloudWatch Logs
+        log.Printf("Gin cold start")
+        r := gin.Default()
+        r.GET("/ping", func(c *gin.Context) {
+                c.JSON(200, gin.H{
+                        "message": "pong",
+                })
+        })
+
+        r.GET("/health", func(c *gin.Context) {
+                c.JSON(200, gin.H{
+                        "message": "health",
+                })
+        })
+
+        ginLambda = ginadapter.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+        // If no name is provided in the HTTP request body, throw an error
+        return ginLambda.ProxyWithContext(ctx, req)
+}
 
 func main() {
-	fmt.Print("Here, from where it starts")
+        lambda.Start(Handler)
 }
