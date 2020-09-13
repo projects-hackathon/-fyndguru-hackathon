181b6ff4dfcdbcaa96a2f2a87400bedc1f093a04
nithishprabhu <nithish.prabhu@games24x7.com>
2020-09-13 20:48:17 +0530

    mongo init url

--- backend/mongopack/mongoInit.go
+++ backend/mongopack/mongoInit.go
@@ -16,7 +16,7 @@ func init() {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
 	//client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://192.168.43.41:27017"))
-	
+
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://adminpass:adminpass@cluster0.d1egb.mongodb.net/fyndguru?retryWrites=true&w=majority"))
 
 	if err != nil {
